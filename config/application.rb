37691d7f1dfc0422f362bcd5bd0f31686b4a1c75
derkach2288 <derkach2288@gmail.com>
2022-09-25 10:36:48 +0300

    dockerizing

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,28 @@ module Navigator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Log to STDOUT because Docker expects all processes to log here. You could
+    # then collect logs using journald, syslog or forward them somewhere else.
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+
+    # Set Redis as the back-end for the cache.
+    config.cache_store = :redis_cache_store, {
+      url: ENV.fetch("REDIS_URL") { "redis://redis:6379/1" },
+      namespace: "cache"
+    }
+
+    # Set Sidekiq as the back-end for Active Job.
+    config.active_job.queue_adapter = :sidekiq
+
+    # Mount Action Cable outside the main process or domain.
+    config.action_cable.mount_path = nil
+    config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "ws://localhost:28080" }
+
+    # Only allow connections to Action Cable from these domains.
+    origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "http:\/\/localhost*" }.split(",")
+    origins.map! { |url| /#{url}/ }
+    config.action_cable.allowed_request_origins = origins
   end
 end
