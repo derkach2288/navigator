0ea56808d98ca04a0517756092d3ff4d57eb6196
derkach2288 <derkach2288@gmail.com>
2022-10-11 13:41:42 +0300

    namespace admin та добавлений контроллер, який вивиодить список всіх юзерів

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    namespace :admin do
+        resources :users, only: [:index]
+    end
+
     devise_for :users, :controllers => {:registrations => "registrations"}
     root to: "pages#index"
     resources :pages
