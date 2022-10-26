9074405ab3a208bf632c5d57d54e7f9352487d09
derkach2288 <derkach2288@gmail.com>
2022-10-26 14:42:34 +0300

    доступ до списку юзерів має лише адмін

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,10 @@
 class Admin::UsersController < ApplicationController
+    
+    before_action :authenticate_user!
+
     def index
+        
         @users = User.all
+        authorize @users
     end
 end
