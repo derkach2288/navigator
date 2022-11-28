501a660315ef301a39478d552b83e0f204f1a78d
derkach2288 <derkach2288@gmail.com>
2022-11-28 11:50:12 +0200

    відключив реєстрацію devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  def new
+    redirect_to new_user_session_path and return
+    super
+  end
+
     protected
   
+
     def after_sign_up_path_for(resource)
       root_path # Or :prefix_to_your_route
     end
