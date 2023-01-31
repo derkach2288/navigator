e5e9f869eff1913d2c57db5fc0ebac966af00301
derkach2288 <derkach2288@gmail.com>
2023-01-31 15:32:10 +0200

    переніс інвайти до адмінської секції

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,29 +2,28 @@ class InvitationsController < Devise::InvitationsController
     before_action :configure_permitted_parameters
     before_action :authenticate_user!
     
- layout :set_layout
+#  layout :set_layout
 
 
     def new
-      authorize User
-      super
+    redirect_to admin_invitation_path and return
+    super
     end
      protected
 
   # Permit the new params here.
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: [:role])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :company_id])
   end
 
-  def set_layout
-    if current_user.nil?
-      'application'
-    elsif current_user.admin?
-        'admin'
-    else
-      'application'
-    end
-  end
+  # def set_layout
+  #   if current_user.nil?
+  #     'application'
+  #   elsif current_user.admin?
+  #       'admin'
+  #   else
+  #     'application'
+  #   end
+  # end
  
 end
