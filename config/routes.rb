df46f2eba56e0ec73251612172ba2125ee306eee
derkach2288 <derkach2288@gmail.com>
2023-02-13 12:46:43 +0200

    створив скафолд категорій

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
     namespace :admin do
         devise_scope :user do
             get 'invitation', to: 'invitations#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_124637) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_120403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
